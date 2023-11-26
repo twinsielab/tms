658b2728d0ef0687950cdc5df7750c75e34c0f23
twinsielab <148714701+twinsielab@users.noreply.github.com>
2023-11-26 05:10:11 -0300

    tidy up settings

--- TInyModTMS/config_example.h
+++ TInyModTMS/config_example.h
@@ -4,38 +4,36 @@
 Slot slots[MAX_SLOTS] = {
   // SLOT-1
   {
-    6,        // selectorInputPin  (filament detector pin in the selector)
-    7,        // feederEnablePin    (driver enable pin)
-    9,        // feederDirPin       (driver direction pin)
-    8,        // feederStepPin      (driver step pin)
-    LOW,      // feedDir            (direction which the motor feeds)
-    100 / 2,  // feederStepsPerMM
-    7,        // spoolEnablePin    (driver enable pin)
-    9,        // spoolDirPin       (driver direction pin)
-    8,        // spoolStepPin      (driver step pin)
-    LOW,      // spooldDir         (direction which the motor feeds)
-    100 / 2,  // spoolStepsPerMM
+     5,               // selectorInputPin    filament detector pin in the selector
+     6,               // feederEnablePin     feeder driver enable pin
+     7,               // feederDirPin        feeder driver direction pin
+     8,               // feederStepPin       feeder driver step pin
+     9,               // spoolEnablePin      spool driver enable pin
+    10,               // spoolDirPin         spool driver direction pin
+    11,               // spoolStepPin        spool driver step pin
+    LOW,              // feedDir             direction which the feeder motor feeds ↓
+    LOW,              // spooldDir           direction which the spool motor unwinds⤵︎
+    100,              // feederStepsPerMM
+    (200 * 16) / 70,  // spoolStepsPerMM
   },
 };
 
 // Global Settings
-#define SPOOL_EXTRA_REWIND 150  // rewind the spool by an extra amount after unloading
-#define JERKY_SYNC_LENGTH 10
-
-// Motor Settings
-#define MOTOR_ON LOW             // state which the motor is turned on by the driver
-#define LOAD_SPEED 400           // speed (mm/s) for the loading filament sequence 
-#define UNLOAD_SPEED 400         // speed (mm/s) for the unloading filament sequence
+#define LOAD_SPEED 400             // speed (mm/s) for the loading filament sequence 
+#define UNLOAD_SPEED 400           // speed (mm/s) for the unloading filament sequence
+#define SPOOL_EXTRA_REWIND 150     // rewind the spool by an extra distance (mm) after unloading
+#define MOTOR_ON LOW               // state which the motor is turned on by the driver
+#define JERKY_SYNC_LENGTH 30
 
 // Selector Settings
-#define SELECTOR_END_PIN 1            // filament detector pin at the end of selector
-#define SELECTOR_DETECT_LEVEL LOW     // level when the Selector has filament
-#define SELECTOR_OFFSET_BEFORE 30     // how much to retract before the sensor
-#define SELECTOR_OFFSET_AFTER  95     // how much to feed after the sensor
+#define SELECTOR_END_PIN 3          // filament detector pin at the end of selector
+#define SELECTOR_DETECT_LEVEL LOW   // level when the Selector has filament
+#define SELECTOR_OFFSET_BEFORE 30   // how much to retract before the sensor
+#define SELECTOR_OFFSET_AFTER  95   // how much to feed after the sensor
 
 // Buffer Settings
-#define BUFFER_PIN 4
-#define BUFFER_EMPTY_LEVEL LOW   // level when the Buffer is empty
-#define FEED_SPEED 200           // speed (mm/s) which filament will be feed when theres no pressure on the buffer
-#define PRELOAD_SPEED 100        // speed (mm/s) which the buffer spring will be compressed
-#define BUFFER_PRELOAD_LENGH 15  // how much preload the buffer spring will be kept compressed
+#define BUFFER_PIN 4                // pressure detection pin on the buffer
+#define BUFFER_EMPTY_LEVEL LOW      // level when the Buffer is empty
+#define FEED_SPEED 200              // speed (mm/s) which filament will be feed when theres no pressure on the buffer
+#define PRELOAD_SPEED 100           // speed (mm/s) which the buffer spring will be compressed
+#define BUFFER_PRELOAD_LENGH 15     // how much preload the buffer spring will be compressed
--- TInyModTMS/types.h
+++ TInyModTMS/types.h
@@ -1,22 +1,16 @@
 // Define a struct to hold slot information
 struct Slot {
-  uint8_t selectorInputPin; // selectorInputPin (filament detector in the selector slot)
-
-  uint8_t feederEnablePin; // feederEnablePin  (driver enable pin)
-  uint8_t feederDirPin; // feederDirPin (driver direction pin)
-  uint8_t feederStepPin; // feederStepPin  (driver step pin)
-
-  bool feedDir; // feedDir  (direction which the motor feeds LOW/HIGH)
-  float feederStepsPerMM; // feederStepsPerMM
-
-
-  uint8_t spoolEnablePin; // spoolEnablePin  (driver enable pin)
-  uint8_t spoolDirPin; // spoolDirPin (driver direction pin)
-  uint8_t spoolStepPin; // spoolStepPin  (driver step pin)
-
-  bool spoolDir; // feedDir  (direction which the motor feeds LOW/HIGH)
-  float spoolStepsPerMM; // spoolStepsPerMM
-  
+  uint8_t selectorInputPin; // selectorInputPin    filament detector pin in the selector
+  uint8_t feederEnablePin;  // feederEnablePin     feeder driver enable pin
+  uint8_t feederDirPin;     // feederDirPin        feeder driver direction pin
+  uint8_t feederStepPin;    // feederStepPin       feeder driver step pin
+  uint8_t spoolEnablePin;   // spoolEnablePin      spool driver enable pin
+  uint8_t spoolDirPin;      // spoolDirPin         spool driver direction pin
+  uint8_t spoolStepPin;     // spoolStepPin        spool driver step pin
+  bool feedDir;             // feedDir             direction which the feeder motor feeds ↓
+  bool spoolDir;            // spooldDir           direction which the spool motor unwinds⤵︎
+  float feederStepsPerMM;   // feederStepsPerMM
+  float spoolStepsPerMM;    // spoolStepsPerMM
 };
 
 
