98a9480da8cbc96508b745568889ac80cb227ce2
twinsielab <148714701+twinsielab@users.noreply.github.com>
2023-11-26 18:47:21 -0300

    delay between detect and load

--- TInyModTMS/main.ino
+++ TInyModTMS/main.ino
@@ -286,6 +286,7 @@ void refill() {
      for (uint8_t i = 1; i <= MAX_SLOTS; i++) {
       if (Selector::inputHasFilament(i)==false && slotHasFilament(i)) {
         Serial.println("Filament detected!");
+        delay(1000);
         loadSlot(i);
         // unloadSlot(i);
         return;
